d4817cba244eef958783029f02b22b6d554ca3da
steve <chph13420146901@gmail.com>
2018-07-05 10:14:38 +0800

    配置好了CMake和Clion环境，算法，我来了！！

---
+++ test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by steve on 18-7-5.
+//
+
+#include "test.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Algorithm!" << endl;
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,8 @@
+//
+// Created by steve on 18-7-5.
+//
+
+#ifndef ALGORITHMCPP_TEST_H
+#define ALGORITHMCPP_TEST_H
+
+#endif //ALGORITHMCPP_TEST_H
