d41cb54562cb7472ac04665ec2b7137ed7dbcdf6
steve <chph13420146901@gmail.com>
2018-08-12 21:05:59 +0800

    第二部分 语言篇 第六章 6.1.1源码

---
+++ Part2/Chapter6/6_1_1.cpp
@@ -0,0 +1,25 @@
+//
+// Created by steve on 18-8-12.
+//
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<int> q;
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        q.push(i+1);
+    }
+    while (!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    return 0;
+}
