2715e7d93d9e43b83edb0e952991e2b3ff32f691
steve <chph13420146901@gmail.com>
2018-08-31 11:14:21 +0800

    例题6-5 移动盒子（Boxes in a Line, UVa 12657）
    这是一份会超时的代码

---
+++ Version2/Chapter6/Exam_6_5.cpp
@@ -0,0 +1,77 @@
+//
+// Created by steve on 18-8-31.
+//
+
+#include <iostream>
+
+void bind(int l, int r);
+
+int *left;
+int *right;
+
+int main() {
+    int n, m;
+    int i = 1;
+
+    while (scanf("%d %d", &n, &m) == 2) {
+        left = (int *) malloc(sizeof(int) * (n + 1));
+        right = (int *) malloc(sizeof(int) * (n + 1));
+        for (int i = 1; i <= n; ++i) {
+            left[i] = i - 1;
+            right[i] = i + 1;
+        }
+        left[0] = n;
+        right[0] = 1;
+        right[n] = 0;
+
+        while (m--) {
+            int type, X, Y;
+            scanf("%d", &type);
+            if (type == 1) {
+                scanf("%d %d", &X, &Y);
+                right[left[X]] = right[X];
+                left[right[X]] = left[X];
+                bind(left[Y], X);
+                bind(X, Y);
+            } else if (type == 2) {
+                scanf("%d %d", &X, &Y);
+                right[left[X]] = right[X];
+                left[right[X]] = left[X];
+                bind(X, right[Y]);
+                bind(Y, X);
+            } else if (type == 3) {
+                scanf("%d %d", &X, &Y);
+                right[left[X]] = Y;
+                right[left[Y]] = X;
+                int temp = left[X];
+                left[X] = left[Y];
+                left[Y] = temp;
+
+                left[right[X]] = Y;
+                left[right[Y]] = X;
+                temp = right[X];
+                right[X] = right[Y];
+                right[Y] = temp;
+            } else {
+                for (int i = 0; i <= n; ++i) {
+                    int temp = left[i];
+                    left[i] = right[i];
+                    right[i] = temp;
+                }
+            }
+        }
+        long long result = 0;
+        int cur = right[0];
+        for (int j = 1; j <= n; ++j) {
+            if (j % 2) result += cur;
+            cur = right[cur];
+        }
+        printf("Case %d: %lli\n", i++, result);
+    }
+    return 0;
+}
+
+void bind(int l, int r) {
+    right[l] = r;
+    left[r] = l;
+}
