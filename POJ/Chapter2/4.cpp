424b671589affba96371f9e2dc7ef730f3ab6022
steve <chph13420146901@gmail.com>
2018-09-20 14:35:54 +0800

    POJ 第二章
    第四题
    
    思路：其实这根将一个整数m分成n个整数之和是类似的。
    设f[m][n]为将m分成最多n份的方案数，且其中的方案不重复，即每个方案前一个份的值一定不会比后面的大。
    则有：
    f[m][n] = f[m][n - 1] + f[m - n][n];
               = 1 // m== 0 || n == 1
               = 0 // m < 0
    f[m][n - 1]相当于第一盘子中为0，只用将数分成n - 1份即可。因为0不会大于任何数，相当于f[m][n - 1]中的方案前面加一个为0的盘子，而且不违背f的定义。所以f[m][n - 1]一定是f[m][n]的方案的一部分，即含有0的方案数。
    f[m - n][n]相当于在每个盘子中加一个数1。因为每个盘子中加一个数1不会影响f[m][n - 1]中的方案的可行性，也不会影响f的定义。所以f[m - n][n]一定是f[m][n]的方案的一部分，即不含有0的方案数。

--- POJ/Chapter2/4.cpp
+++ POJ/Chapter2/4.cpp
@@ -7,29 +7,19 @@
 
 using namespace std;
 
+int solution(int m, int n) {
+    if (m < 0) return 0;
+    else if (m == 0|| n == 1) return 1;//找到一个方案
+    return solution(m, n - 1) + solution(m - n, n);//有0的情况和没有0的情况
+}
+
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int M, N, K = 1;
+        int M, N;
         cin >> M >> N;
-        int boxes[N];
-        memset(boxes,0, sizeof(boxes));
-        boxes[0] = M;
-        while (true){
-            bool can_move = false;
-            for (int i = N-1; i >0; --i) {
-                 if (boxes[i-1]-boxes[i]>=2){
-                    boxes[i-1]--;
-                    boxes[i]++;
-                    can_move = true;
-                    K++;
-                    break;
-                } else if (boxes[i-1]-boxes[i] >= 0) continue;
-            }
-            if (!can_move) break;
-        }
-        cout << K << endl;
+        cout << solution(M,N) << endl;
     }
     return 0;
 }
