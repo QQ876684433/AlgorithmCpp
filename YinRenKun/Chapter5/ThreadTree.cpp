11cfd5976117b8a8b578f84ef518ecade2d56965
steve <chph13420146901@gmail.com>
2018-10-15 21:15:01 +0800

    Thread Binary Tree
    线索二叉树 adt实现

--- YinRenKun/Chapter5/ThreadTree.cpp
+++ YinRenKun/Chapter5/ThreadTree.cpp
@@ -44,3 +44,107 @@ public:
 
     void PostOrder(void (*visit)(ThreadNode<T> *p));
 };
+
+template<typename T>
+ThreadNode<T> *ThreadTree<T>::First(ThreadNode<T> *current) {
+//    返回中序线索二叉树的第一个结点
+    ThreadNode<T> *p = current;
+    while (p->ltag == 0)p = p->leftChild;
+    return p;
+}
+
+template<typename T>
+ThreadNode<T> *ThreadTree<T>::Next(ThreadNode<T> *current) {
+//    返回current在中序序列下的后继结点
+    ThreadNode<T> *p = current->rightChild;
+    if (current->rtag == 0)return First(p);//在右子树中找中序下的第一个结点
+    else return p;//rtag==1，直接返回后继搜索
+}
+
+template<typename T>
+ThreadNode<T> *ThreadTree<T>::Last(ThreadNode<T> *current) {
+    ThreadNode<T> *p = current;
+    while (p->rtag == 0)p = p->rightChild;
+    return p;
+}
+
+template<typename T>
+ThreadNode<T> *ThreadTree<T>::Prior(ThreadNode<T> *current) {
+//    返回current在中序下的前驱结点
+    ThreadNode<T> *p = current->leftChild;
+    if (p->ltag == 0)return Last(p);
+    else return p;
+}
+
+template<typename T>
+void ThreadTree<T>::InOrder(void (*visit)(ThreadNode<T> *)) {
+    ThreadNode<T> *p;
+    for (p = First(root); p != NULL; p = Next(p))visit(p);
+}
+
+template<typename T>
+void ThreadTree<T>::CreateInThread() {
+    ThreadNode<T> *pre;//前驱结点指针
+    if (root != NULL) {//非空二叉树，开始线索化
+        CreateInThread(root, pre);
+        pre->rightChild = NULL;
+        pre->rtag = 1;
+    }
+}
+
+template<typename T>
+void ThreadTree<T>::CreateInThread(ThreadNode<T> *current, ThreadNode<T> *&pre) {
+    if (!current)return;
+    CreateInThread(current->leftChild, pre);//左子树线索化
+    if (!current->leftChild) {//建立当前结点的前驱线索
+        current->leftChild = pre;
+        current->ltag = 1;
+    }
+    if (pre && !pre->rightChild) {//建立当前结点的后继线索
+        pre->rightChild = current;
+        pre->rtag = 1;
+    }
+    pre = current;//前驱跟上
+    CreateInThread(current->rightChild, pre);//右子树线索化
+}
+
+template<typename T>
+void ThreadTree<T>::PreOrder(void (*visit)(ThreadNode<T> *)) {
+    ThreadNode<T> *p = root;
+    while (p != NULL) {
+        visit(p);
+        if (p->ltag == 0) p = p->leftChild;//有左子女，则为p的后继
+        else if (p->rtag == 0)p = p->rightChild;//否则如果有右子女，则为p的后继
+        else {//否则，寻找有右子女的结点
+            while (p != NULL && p->rtag == 1) p = p->rightChild;//知道有右子女的结点出现
+            if (p != NULL) p = p->rightChild;
+        }
+    }
+}
+
+template<typename T>
+void ThreadTree<T>::PostOrder(void (*visit)(ThreadNode<T> *)) {
+    ThreadNode<T> *t = root;
+    while (t->ltag == 0 || t->rtag == 0) {//寻找第一个结点
+        if (t->ltag == 0)t = t->leftChild;
+        else if (t->rtag == 0)t = t->rightChild;
+    }
+    visit(t);
+    ThreadNode<T> *p;//父结点
+    while ((p = Parent(t)) != NULL) {
+        if (p->rightChild == t || p->rtag == 1)t = p;
+        else {
+            t = p->rightChild;//t移到p的右子树
+            while (t->ltag == 0 || t->rtag == 0) {
+                if (t->ltag == 0)t = t->leftChild;
+                else if (t->rtag == 0)t = t->rightChild;
+            }
+        }
+        visit(t);
+    }
+}
+
+template<typename T>
+ThreadNode<T> *ThreadTree<T>::Parent(ThreadNode<T> *t) {
+
+}
