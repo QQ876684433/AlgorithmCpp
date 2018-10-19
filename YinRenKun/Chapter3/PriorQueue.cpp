36da98e49223d1aef0faa957f3ad210eaf814b10
steve <chph13420146901@gmail.com>
2018-10-19 17:08:26 +0800

    优先级队列 函数实现

--- YinRenKun/Chapter3/PriorQueue.cpp
+++ YinRenKun/Chapter3/PriorQueue.cpp
@@ -13,15 +13,15 @@ const int DefaultPQSize = 50;
 template<typename T>
 class PriorQueue {
 protected:
-    T *qp_elements;//优先级队列数组
+    T *pq_elements;//优先级队列数组
     int count;//当前元素个数
     int maxSize;//最大可容纳元素个数
-    void adjust();//队列调整
+    void adjust();//按优先权级进行队列调整
 
 public:
     PriorQueue(int sz = DefaultPQSize);
 
-    ~PriorQueue() { delete (qp_elements); }
+    ~PriorQueue() { delete (pq_elements); }
 
     bool Insert(const T &x);
 
@@ -37,4 +37,45 @@ public:
 
     int GetSize() const { return count; }
 };
+
+template<typename T>
+PriorQueue<T>::PriorQueue(int sz):maxSize(sz), count(0) {
+    pq_elements = new T[maxSize];
+    assert(pq_elements != NULL);
+}
+
+template<typename T>
+bool PriorQueue<T>::Insert(const T &x) {
+    if (count == maxSize)return false;
+    pq_elements[count++] = x;
+    adjust();
+    return true;
+}
+
+template<typename T>
+void PriorQueue<T>::adjust() {
+    T temp = pq_elements[count - 1];
+    int j;
+    for (j = count - 2; j >= 0; j--) {
+        if (pq_elements[j] <= temp)break;
+        pq_elements[j + 1] = pq_elements[j];
+    }
+    pq_elements[j + 1] = temp;
+}
+
+template<typename T>
+bool PriorQueue<T>::RemoveMin(T &x) {
+    if (count == 0)return false;
+    x = pq_elements[0];
+    for (int i = 1; i < count; ++i) pq_elements[i - 1] = pq_elements[i];
+    count--;
+    return true;
+}
+
+template<typename T>
+bool PriorQueue<T>::GetFront(T &x) const {
+    if (count == 0)return false;
+    x = pq_elements[0];
+    return true;
+}
 
