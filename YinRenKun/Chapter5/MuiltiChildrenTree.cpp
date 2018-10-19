e4c6e7f68c16445bb63483666bfff44eba24e9da
steve <chph13420146901@gmail.com>
2018-10-19 16:22:43 +0800

    树的表示——子女-兄弟链表表示法

--- YinRenKun/Chapter5/MuiltiChildrenTree.cpp
+++ YinRenKun/Chapter5/MuiltiChildrenTree.cpp
@@ -54,17 +54,70 @@ bool Tree<T>::Root() {
 
 template<typename T>
 bool Tree<T>::Parent() {
-    if (current==NULL||current==root) {
-        current=NULL;
+    if (current == NULL || current == root) {
+        current = NULL;
         return false;
-    } else{
-        TreeNode<T>*p=current;
-        current=FindParent(root,p);
+    } else {
+        TreeNode<T> *p = current;
+        current = FindParent(root, p);
         return true;
     }
 }
 
 template<typename T>
 bool Tree<T>::FindParent(TreeNode<T> *t, TreeNode<T> *p) {
+    TreeNode<T> *q = t->firstChild;
+    bool succ;
+    while (q != NULL && q != p) {
+        if ((succ = FindParent(q, p)) == true) { return succ; }
+        q = q->nextSibling;
+    }
+    if (q != NULL && q == p) {
+        current = t;
+        return true;
+    } else {
+        current = NULL;
+        return false;
+    }
+}
 
+template<typename T>
+bool Tree<T>::FirstChild() {
+    if (current != NULL && current->firstChild != NULL) {
+        current = current->firstChild;
+        return true;
+    }
+    current = NULL;
+    return false;
+}
+
+template<typename T>
+bool Tree<T>::NextSibling() {
+    if (current && current->nextSibling) {
+        current = current->nextSibling;
+        return true;
+    }
+    current = NULL;
+    return false;
+}
+
+template<typename T>
+bool Tree<T>::Find(T target) {
+    if (IsEmpty())return false;
+    else return FindParent(root, target);
+}
+
+template<typename T>
+bool Tree<T>::Find(TreeNode<T> *p, T value) {
+    bool succ = false;
+    if (p->data == value) {
+        succ = true;
+        current = p;
+    } else {
+        TreeNode<T> *q = p->firstChild;
+        while (q != NULL && !(succ = FindParent(q, value))) {
+            q = q->nextSibling;
+        }
+    }
+    return succ;
 }
